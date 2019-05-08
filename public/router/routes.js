f7689512aca42ac4cf5b0b858a0fe96c1fa0a1dd
2ndyrGroupA <2ndyrGroupA@pn.local>
2019-05-08 19:16:14 +0800

    files modified

--- public/router/routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app){
-    var path = require('path');
-    app.get('/',function(req,res){
-        res.sendFile(path.join('../views/login.html'));
-    })
-}
-
--- server.js
+++ server.js
@@ -3,21 +3,22 @@ const app = express();
 const server = require('http').createServer(app); 
 
 const mysql = require('./public/scripts/mysql');
-const routes = require('./public/router/routes.js');
+const routes = require('./router/routes.js');
 
 const port = process.env.PORT||3000;
 app.use(express.static("public"));
+
 //app.use('/public', routes);
 //app.set('view',__dirname+'/pulic/views');
-//app.set("view engine", "ejs");
-//routes(app);
+app.set("view engine", "ejs");
+routes(app);
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname +"/public/views/login.html");
-});
-app.get('/dashboard', function(req, res){
-    res.sendFile(__dirname +"/public/views/dashboard.html");
-});
+// app.get('/', function(req, res){
+//     res.sendFile(__dirname +"/public/views/login.html");
+// });
+// app.get('/dashboard', function(req, res){
+//     res.sendFile(__dirname +"/public/views/dashboard.html");
+// });
 
 //Connecting to database before running the server
 mysql.connect(function(err){
