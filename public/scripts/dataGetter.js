7ec892d80f50c4ed51b0f536b446cffbe26e06ce
ALINGASA <alingasada_sd203a1@ceaccs.net>
2019-05-15 12:58:59 +0800

    files modified

--- public/scripts/dataGetter.js
+++ public/scripts/dataGetter.js
@@ -2,7 +2,7 @@ module.exports = function(mysql){
  var fs = require('fs');
   mysql.connect(function(err){
       // Kitchen
-      mysql.query(`SELECT * FROM tblCurrentTasking where TaskName = 'Kitchen'`, function (err, result) {
+      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Kitchen'`, function (err, result) {
         if (err) {
             console.log("Error neh")
         }
@@ -11,7 +11,7 @@ module.exports = function(mysql){
         fs.writeFile('./public/scripts/records/Kitchen.json',data,finished);
       });
       // DishWasher
-      mysql.query(`SELECT * FROM tblCurrentTasking where TaskName = 'DishWasher'`, function (err, result) {
+      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'DishWashing'`, function (err, result) {
         if (err) {
             console.log("Error neh")
         }
@@ -19,7 +19,7 @@ module.exports = function(mysql){
         fs.writeFile('./public/scripts/records/DishWasher.json',data,finished);
       });
       // Dining
-      mysql.query(`SELECT * FROM tblCurrentTasking where TaskName = 'Dining'`, function (err, result) {
+      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Dining'`, function (err, result) {
         if (err) {
             console.log("Error neh")
         }
@@ -27,7 +27,7 @@ module.exports = function(mysql){
         fs.writeFile('./public/scripts/records/Dining.json',data,finished);
       });
       // Front Garden
-      mysql.query(`SELECT * FROM tblCurrentTasking where TaskName = 'Front Garden'`, function (err, result) {
+      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Front Garden'`, function (err, result) {
         if (err) {
             console.log("Error neh")
         }
@@ -35,12 +35,68 @@ module.exports = function(mysql){
         fs.writeFile('./public/scripts/records/Front Garden.json',data,finished);
       });
       // Back Garden
-      mysql.query(`SELECT * FROM tblCurrentTasking where TaskName = 'Back Garden'`, function (err, result) {
+      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Back Garden'`, function (err, result) {
         if (err) {
             console.log("Error neh")
         }
         var data = JSON.stringify(result);
         fs.writeFile('./public/scripts/records/Back Garden.json',data,finished);
+      });
+      // Window 1
+      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Window 1'`, function (err, result) {
+        if (err) {
+            console.log("Error neh")
+        }
+        var data = JSON.stringify(result);
+        fs.writeFile('./public/scripts/records/Window 1.json',data,finished);
+      });
+      // Window 2
+      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Window 2'`, function (err, result) {
+        if (err) {
+            console.log("Error neh")
+        }
+        var data = JSON.stringify(result);
+        fs.writeFile('./public/scripts/records/Window 2.json',data,finished);
+      });
+      // Courtyard & Stage
+      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Courtyard & Stage'`, function (err, result) {
+        if (err) {
+            console.log("Error neh")
+        }
+        var data = JSON.stringify(result);
+        fs.writeFile('./public/scripts/records/Courtyard & Stage.json',data,finished);
+      });
+       // Hallways & Stairs
+       mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Hallways & Stairs'`, function (err, result) {
+        if (err) {
+            console.log("Error neh")
+        }
+        var data = JSON.stringify(result);
+        fs.writeFile('./public/scripts/records/Hallways & Stairs.json',data,finished);
+      });
+      // Office
+      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Office'`, function (err, result) {
+        if (err) {
+            console.log("Error neh")
+        }
+        var data = JSON.stringify(result);
+        fs.writeFile('./public/scripts/records/Office.json',data,finished);
+      });
+      // Laundry Area
+      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Laundry Area'`, function (err, result) {
+        if (err) {
+            console.log("Error neh")
+        }
+        var data = JSON.stringify(result);
+        fs.writeFile('./public/scripts/records/Laundry Area.json',data,finished);
+      });
+      // Comlab
+      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Comlab'`, function (err, result) {
+        if (err) {
+            console.log("Error neh")
+        }
+        var data = JSON.stringify(result);
+        fs.writeFile('./public/scripts/records/Comlab.json',data,finished);
       });
   })
   function finished(){
--- public/scripts/mysql.js
+++ public/scripts/mysql.js
@@ -6,16 +6,4 @@ module.exports = mysql.createConnection({
     database: "taskking"
   });
 
-//   con.connect(function(err){
-//       if(err){
-//           console.log("error");
-//       }
-// //    var file = con.query('Select * from tblEducator');
-// //           console.log(file[0].Username);
-// //      for(i = 0;i<file.length;i++){
-// //          console.log(file[i].Username);
-// //      }
-//   })
-
-
   
