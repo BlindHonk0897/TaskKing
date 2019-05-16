d740b445f41315af6e8d677c93d99c7f6cd22bb9
2ndyrGroupA <2ndyrGroupA@pn.local>
2019-05-17 06:42:26 +0800

    files modified

--- public/controller/shuffleStudent.js
+++ public/controller/shuffleStudent.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+   $(".preloader").hide();
+   $("#download").hide();
     $shuffle = $('#shuffle');
     $myModal = $('#myModal');
     $today = new Date();
@@ -7,15 +9,40 @@ $(document).ready(function(){
           alert("It is not yet time to shuffle Tasking!");
           //$('#myModal').modal();
       }else{
-        $.ajax({
-          method:'POST',
-          url:'/shuffle',
-          data:{},
-          success:function(dat){
-             console.log(dat);
-             top.location.href = "/shuffledTask";
-           }
-         })
+        $(".preloader").show();
+        $(".preloader").delay(2000);
+        $(".preloader").fadeOut(function(){
+            //$("#teybol").show();
+            //$("#download").show();
+            //$("#shuffle").disable();
+            $.ajax({
+              method:'POST',
+              url:'/shuffle',
+              data:{},
+              success:function(dat){
+                 console.log(dat);
+                 top.location.href = "/shuffledTask";
+               }
+             })
+        });
+        
       }
     });
+    // $(".preloader").hide();
+    //         $("#download").hide();
+    //          //$("#teybol").hide();
+    //          $("#shuffle").click(function(){
+    //              $(".preloader").show();
+    //              $(".preloader").delay(2000);
+    //              $(".preloader").fadeOut(function(){
+    //                  //$("#teybol").show();
+    //                  //$("#download").show();
+    //                  //$("#shuffle").disable();
+    //              });
+                 
+    //            //   $("#download").show();
+    //          });
+    $("#download").click(function(){
+      window.print();
+    });       
 })
