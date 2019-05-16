f6ecac33502d5d75ef2154f673803e9b586e862d
2ndyrGroupA <2ndyrGroupA@pn.local>
2019-05-16 08:37:11 +0800

    files modified

--- public/scripts/dataGetter.js
+++ public/scripts/dataGetter.js
@@ -1,121 +1,123 @@
 module.exports = function(mysql){
- var fs = require('fs');
-  mysql.connect(function(err){
-      // Kitchen
-      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Kitchen'`, function (err, result) {
-        if (err) {
-            console.log("Error neh")
-        }
-        var data = JSON.stringify(result);
-        fs.writeFile('./public/scripts/records/Kitchen.json',data,finished);
-      });
-      // DishWasher
-      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'DishWashing'`, function (err, result) {
-        if (err) {
-            console.log("Error neh")
-        }
-        var data = JSON.stringify(result);
-        fs.writeFile('./public/scripts/records/DishWasher.json',data,finished);
-      });
-      // Dining
-      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Dining'`, function (err, result) {
-        if (err) {
-            console.log("Error neh")
-        }
-        var data = JSON.stringify(result);
-        fs.writeFile('./public/scripts/records/Dining.json',data,finished);
-      });
-      // Front Garden
-      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Front Garden'`, function (err, result) {
-        if (err) {
-            console.log("Error neh")
-        }
-        var data = JSON.stringify(result);
-        fs.writeFile('./public/scripts/records/Front Garden.json',data,finished);
-      });
-      // Back Garden
-      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Back Garden'`, function (err, result) {
-        if (err) {
-            console.log("Error neh")
-        }
-        var data = JSON.stringify(result);
-        fs.writeFile('./public/scripts/records/Back Garden.json',data,finished);
-      });
-      // Window 1
-      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Window 1'`, function (err, result) {
-        if (err) {
-            console.log("Error neh")
-        }
-        var data = JSON.stringify(result);
-        fs.writeFile('./public/scripts/records/Window 1.json',data,finished);
-      });
-      // Window 2
-      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Window 2'`, function (err, result) {
-        if (err) {
-            console.log("Error neh")
-        }
-        var data = JSON.stringify(result);
-        fs.writeFile('./public/scripts/records/Window 2.json',data,finished);
-      });
-      // Courtyard & Stage
-      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Courtyard & Stage'`, function (err, result) {
-        if (err) {
-            console.log("Error neh")
-        }
-        var data = JSON.stringify(result);
-        fs.writeFile('./public/scripts/records/Courtyard & Stage.json',data,finished);
-      });
-       // Hallways & Stairs
-       mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Hallways & Stairs'`, function (err, result) {
-        if (err) {
-            console.log("Error neh")
-        }
-        var data = JSON.stringify(result);
-        fs.writeFile('./public/scripts/records/Hallways & Stairs.json',data,finished);
-      });
-      // Office
-      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Office'`, function (err, result) {
-        if (err) {
-            console.log("Error neh")
-        }
-        var data = JSON.stringify(result);
-        fs.writeFile('./public/scripts/records/Office.json',data,finished);
-      });
-      // Laundry Area
-      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Laundry Area'`, function (err, result) {
-        if (err) {
-            console.log("Error neh")
-        }
-        var data = JSON.stringify(result);
-        fs.writeFile('./public/scripts/records/Laundry Area.json',data,finished);
-      });
-      // Comlab
-      mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Comlab'`, function (err, result) {
-        if (err) {
-            console.log("Error neh")
-        }
-        var data = JSON.stringify(result);
-        fs.writeFile('./public/scripts/records/Comlab.json',data,finished);
-      });
+  var fs = require('fs');
+   mysql.connect(function(err){
+       // Kitchen
+       mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Kitchen'`, function (err, result) {
+         if (err) {
+             console.log("Error neh")
+         }
+         var data = JSON.stringify(result);
+         fs.writeFile('./public/scripts/records/Kitchen.json',data,finished);
+       });
+       // DishWasher
+       mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'DishWashing'`, function (err, result) {
+         if (err) {
+             console.log("Error neh")
+         }
+         var data = JSON.stringify(result);
+         fs.writeFile('./public/scripts/records/DishWasher.json',data,finished);
+       });
+       // Dining
+       mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Dining'`, function (err, result) {
+         if (err) {
+             console.log("Error neh")
+         }
+         var data = JSON.stringify(result);
+         fs.writeFile('./public/scripts/records/Dining.json',data,finished);
+       });
+       // Front Garden
+       mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Front Garden'`, function (err, result) {
+         if (err) {
+             console.log("Error neh")
+         }
+         var data = JSON.stringify(result);
+         fs.writeFile('./public/scripts/records/Front Garden.json',data,finished);
+       });
+       // Back Garden
+       mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Back Garden'`, function (err, result) {
+         if (err) {
+             console.log("Error neh")
+         }
+         var data = JSON.stringify(result);
+         fs.writeFile('./public/scripts/records/Back Garden.json',data,finished);
+       });
+       // Window 1
+       mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Window 1'`, function (err, result) {
+         if (err) {
+             console.log("Error neh")
+         }
+         var data = JSON.stringify(result);
+         fs.writeFile('./public/scripts/records/Window 1.json',data,finished);
+       });
+       // Window 2
+       mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Window 2'`, function (err, result) {
+         if (err) {
+             console.log("Error neh")
+         }
+         var data = JSON.stringify(result);
+         fs.writeFile('./public/scripts/records/Window 2.json',data,finished);
+       });
+       // Courtyard & Stage
+       mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Courtyard & Stage'`, function (err, result) {
+         if (err) {
+             console.log("Error neh")
+         }
+         var data = JSON.stringify(result);
+         fs.writeFile('./public/scripts/records/Courtyard & Stage.json',data,finished);
+       });
+        // Hallways & Stairs
+        mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Hallways & Stairs'`, function (err, result) {
+         if (err) {
+             console.log("Error neh")
+         }
+         var data = JSON.stringify(result);
+         fs.writeFile('./public/scripts/records/Hallways & Stairs.json',data,finished);
+       });
+       // Office
+       mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Office'`, function (err, result) {
+         if (err) {
+             console.log("Error neh")
+         }
+         var data = JSON.stringify(result);
+         fs.writeFile('./public/scripts/records/Office.json',data,finished);
+       });
+       // Laundry Area
+       mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Laundry Area'`, function (err, result) {
+         if (err) {
+             console.log("Error neh")
+         }
+         var data = JSON.stringify(result);
+         fs.writeFile('./public/scripts/records/Laundry Area.json',data,finished);
+       });
+       // Comlab
+       mysql.query(`SELECT LastName FROM tblCurrentTasking where TaskName = 'Comlab'`, function (err, result) {
+         if (err) {
+             console.log("Error neh")
+         }
+         var data = JSON.stringify(result);
+         fs.writeFile('./public/scripts/records/Comlab.json',data,finished);
+       });
       
-      // Student A
-      mysql.query(`SELECT StudentLastname FROM tblStudents where GroupName = 'A'`, function (err, result) {
-        if (err) {
-            console.log("Error neh")
-        }
-        var data = JSON.stringify(result);
-        fs.writeFile('./public/scripts/records/Student A.json',data,finished);
-      });
-      // Student B
-      mysql.query(`SELECT StudentLastname FROM tblStudents where GroupName = 'B'`, function (err, result) {
-        if (err) {
-            console.log("Error neh")
-        }
-        var data = JSON.stringify(result);
-        fs.writeFile('./public/scripts/records/Student B.json',data,finished);
-      });
-  })
-  function finished(){
-    console.log('finished.');
+       // Student A
+       mysql.query(`SELECT StudentLastname FROM tblStudents where GroupName = 'A'`, function (err, result) {
+         if (err) {
+             console.log("Error neh")
+         }
+         var data = JSON.stringify(result);
+         fs.writeFile('./public/scripts/records/Student A.json',data,finished);
+       });
+       // Student B
+       mysql.query(`SELECT StudentLastname FROM tblStudents where GroupName = 'B'`, function (err, result) {
+         if (err) {
+             console.log("Error neh")
+         }
+         var data = JSON.stringify(result);
+         fs.writeFile('./public/scripts/records/Student B.json',data,finished);
+       });
+   })
+   function finished(){
+     console.log('finished.');
+   }
   }
-}
+  
+  
--- public/scripts/shuffler.js
+++ public/scripts/shuffler.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const mysql = require('./mysql');
+
+var studA = JSON.parse(fs.readFileSync('./public/scripts/records/Student A.json'));
+var studB= JSON.parse(fs.readFileSync('./public/scripts/records/Student B.json'));
+
 var Kitch = JSON.parse(fs.readFileSync('./public/scripts/records/Kitchen.json'));
 var BG = JSON.parse(fs.readFileSync('./public/scripts/records/Back Garden.json'));
 var Comp = JSON.parse(fs.readFileSync('./public/scripts/records/Comlab.json'));
@@ -13,99 +18,474 @@ var win1 = JSON.parse(fs.readFileSync('./public/scripts/records/Window 1.json'))
 var win2 = JSON.parse(fs.readFileSync('./public/scripts/records/Window 2.json'));
 
 
+// Big task kitch,dinn,dish
+
+// var filtered = items.filter(function(item) {
+//     return item.id !== 123; 
+//  });
+
+var newKitch = [] , newdin = [] ,newdish = [], newBG = [] , newcomp =[] , newCourt = [];
+var newOff = [] , newLaun = [] ,newHall = [], newFG= [] , newWin1 =[] , newWin2 = [];
+
+// 10 kitch
+for(var i = 0;i<10;i++){
+   if(i%2==0){
+     while(true){
+     var ln = getRandomFromB();
+     var ifExist = (checkKitch(ln) || checkDish(ln) || checkDin(ln));
+     if(!ifExist){
+       newKitch.push(ln);
+       var studB = studB.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+        break;
+     }
+   }
+   }else{
+       while(true){
+       var ln = getRandomFromA();
+       var ifExist = (checkKitch(ln) || checkDish(ln) || checkDin(ln));
+       if(!ifExist){
+           var studA = studA.filter(function(item) {
+               return item.StudentLastname !== ln; 
+            });
+         newKitch.push(ln);
+         break;
+       }
+   }
+   }
+}
+console.log("Kitchen")
+console.log(newKitch);
+console.log(studA.length);
+console.log(studB.length);
+// dining 10
+for(var i = 0;i<8;i++){
+   if(i%2==0){
+     while(true){
+     var ln = getRandomFromA();
+     var ifExist = (checkKitch(ln) || checkDish(ln) || checkDin(ln));
+     if(!ifExist){
+       newdin.push(ln);
+       var studA = studA.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+        break;
+     }
+   }
+   }else{
+       while(true){
+       var ln = getRandomFromB();
+       var ifExist = (checkKitch(ln) || checkDish(ln) || checkDin(ln));
+       if(!ifExist){
+           var studB = studB.filter(function(item) {
+               return item.StudentLastname !== ln; 
+            });
+         newdin.push(ln);
+         break;
+       }
+    }
+   }
+}
+console.log("Dining")
+console.log(newdin);
+console.log(studA.length);
+console.log(studB.length);
+
+// Dishwasher
+for(var i = 0;i<10;i++){
+   if(i%2==0){
+     while(true){
+     var ln = getRandomFromB();
+     var ifExist = (checkKitch(ln) || checkDish(ln) || checkDin(ln));
+     if(!ifExist){
+       newdish.push(ln);
+       var studB = studB.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+        break;
+     }
+   }
+   }else{
+       while(true){
+       var ln = getRandomFromA();
+       var ifExist = (checkKitch(ln) || checkDish(ln) || checkDin(ln));
+       if(!ifExist){
+           var studA = studA.filter(function(item) {
+               return item.StudentLastname !== ln; 
+            });
+         newdish.push(ln);
+         break;
+       }
+    }
+   }
+}
+console.log("Dishwasher")
+console.log(newdish);
+console.log(studA.length);
+console.log(studB.length);
+
+// Back Garden 4
+for(var i = 0;i<4;i++){
+   if(i%2==0){
+       var ln = getRandomFromA();
+       newBG.push(ln);
+       var studA = studA.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+   }else{
+       var ln = getRandomFromB();
+       newBG.push(ln);
+       var studB = studB.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+   }
+}
+console.log("Back Garden")
+console.log(newBG);
+console.log(studA.length);
+console.log(studB.length);
+
+// complab
+for(var i = 0;i<3;i++){
+   if(i%2==0){
+       var ln = getRandomFromB();
+       newcomp.push(ln);
+       var studB = studB.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+   }else{
+       var ln = getRandomFromA();
+       newcomp.push(ln);
+       var studA = studA.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+   }
+}
+console.log("Complab")
+console.log(newcomp);
+console.log(studA.length);
+console.log(studB.length);
+
+// office
+var ln = getRandomFromA();
+newOff.push(ln);
+var studA = studA.filter(function(item) {
+   return item.StudentLastname !== ln; 
+});
+console.log("Office")
+console.log(newOff);
+console.log(studA.length);
+console.log(studB.length);
+
+// courtyard
+for(var i = 0;i<6;i++){
+   if(i%2==0){
+       var ln = getRandomFromB();
+       newCourt.push(ln);
+       var studB = studB.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+   }else{
+       var ln = getRandomFromA();
+       newCourt.push(ln);
+       var studA = studA.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+   }
+}
+console.log("Courtyard")
+console.log(newCourt);
+console.log(studA.length);
+console.log(studB.length);
+
+// front Garden
+for(var i = 0;i<4;i++){
+   if(i%2==0){
+       var ln = getRandomFromA();
+       newFG.push(ln);
+       var studA = studA.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+   }else{
+       var ln = getRandomFromB();
+       newFG.push(ln);
+       var studB = studB.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+   }
+}
+console.log("Front Garden")
+console.log(newFG);
+console.log(studA.length);
+console.log(studB.length);
+
+// Hallway
+for(var i = 0;i<6;i++){
+   if(i%2==0){
+       var ln = getRandomFromB();
+       newHall.push(ln);
+       var studB = studB.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+   }else{
+       var ln = getRandomFromA();
+       newHall.push(ln);
+       var studA = studA.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+   }
+}
+console.log("HAllway")
+console.log(newHall);
+console.log(studA.length);
+console.log(studB.length);
+
+//Laundry
+for(var i = 0;i<2;i++){
+   if(i%2==0){
+       var ln = getRandomFromA();
+       newLaun.push(ln);
+       var studA = studA.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+   }else{
+       var ln = getRandomFromB();
+       newLaun.push(ln);
+       var studB = studB.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+   }
+}
+console.log("Laundry")
+console.log(newLaun);
+console.log(studA.length);
+console.log(studB.length);
+// Window 1
+for(var i = 0;i<2;i++){
+   if(i%2==0){
+       var ln = getRandomFromB();
+       newWin1.push(ln);
+       var studB = studB.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+   }else{
+       var ln = getRandomFromA();
+       newWin1.push(ln);
+       var studA = studA.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+   }
+}
+console.log("Window 1")
+console.log(newWin1);
+console.log(studA.length);
+console.log(studB.length);
+
+// Window 2
+for(var i = 0;i<2;i++){
+   if(i%2==0){
+       var ln = getRandomFromA();
+       newWin2.push(ln);
+       var studA = studA.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+   }else{
+       var ln = getRandomFromB();
+       newWin2.push(ln);
+       var studB = studB.filter(function(item) {
+           return item.StudentLastname !== ln; 
+        });
+   }
+}
+console.log("Window 2")
+console.log(newWin2);
+console.log(studA.length);
+console.log(studB.length);
+
+var Coordinator = getRandomCoordinator(newKitch);
+newKitch.sort(function(x,y){ return x == Coordinator ? -1 : y == Coordinator ? 1 : 0; });
+console.log("Kitchen with Coordinator")
+console.log(newKitch);
+
+Coordinator = getRandomCoordinator(newdin);
+newdin.sort(function(x,y){ return x == Coordinator ? -1 : y == Coordinator ? 1 : 0; });
+console.log("newDining with Coordinator")
+console.log(newdin);
+
+Coordinator = getRandomCoordinator(newdish);
+newdish.sort(function(x,y){ return x == Coordinator ? -1 : y == Coordinator ? 1 : 0; });
+console.log("newDish with Coordinator")
+console.log(newdish);
+
+Coordinator = getRandomCoordinator(newBG);
+newBG.sort(function(x,y){ return x == Coordinator ? -1 : y == Coordinator ? 1 : 0; });
+console.log("newBG with Coordinator")
+console.log(newBG);
+
+Coordinator = getRandomCoordinator(newCourt);
+newCourt.sort(function(x,y){ return x == Coordinator ? -1 : y == Coordinator ? 1 : 0; });
+console.log("newCourt with Coordinator")
+console.log(newCourt);
+
+Coordinator = getRandomCoordinator(newFG);
+newFG.sort(function(x,y){ return x == Coordinator ? -1 : y == Coordinator ? 1 : 0; });
+console.log("newFG with Coordinator")
+console.log(newFG);
+
+Coordinator = getRandomCoordinator(newHall);
+newHall.sort(function(x,y){ return x == Coordinator ? -1 : y == Coordinator ? 1 : 0; });
+console.log("NewHall with Coordinator")
+console.log(newHall);
+
+Coordinator = getRandomCoordinator(newLaun);
+newLaun.sort(function(x,y){ return x == Coordinator ? -1 : y == Coordinator ? 1 : 0; });
+console.log("newLAun with Coordinator")
+console.log(newLaun);
+
+Coordinator = getRandomCoordinator(newOff);
+newOff.sort(function(x,y){ return x == Coordinator ? -1 : y == Coordinator ? 1 : 0; });
+console.log("newOff with Coordinator")
+console.log(newOff);
+
+Coordinator = getRandomCoordinator(newWin1);
+newWin1.sort(function(x,y){ return x == Coordinator ? -1 : y == Coordinator ? 1 : 0; });
+console.log("newWin1 with Coordinator")
+console.log(newWin1);
+
+Coordinator = getRandomCoordinator(newWin2);
+newWin2.sort(function(x,y){ return x == Coordinator ? -1 : y == Coordinator ? 1 : 0; });
+console.log("newWin2 with Coordinator")
+console.log(newWin2);
+
+Coordinator = getRandomCoordinator(newcomp);
+newcomp.sort(function(x,y){ return x == Coordinator ? -1 : y == Coordinator ? 1 : 0; });
+console.log("newComp with Coordinator")
+console.log(newcomp);
+
+// mysql.connect(function(err){
+  
+//    // mysql.query(`SELECT * FROM tblEducator where Username = '${req.body.username}' and Password = '${req.body.password}'`, function (err, result) {
+//    //     if (err) throw err;
+//    //     if(result.length > 0){
+//    //         res.send("Success")
+//    //     }else{
+//    //         res.send("Failed")
+//    //     }
+//    // });
+// })
+
+// moving to front
+//var first = "role";
+//data.sort(function(x,y){ return x == first ? -1 : y == first ? 1 : 0; });
+
+function getRandomCoordinator(Task){
+    return Task[Math.floor(Math.random()*Task.length)];
+}
+
+function getRandomFromA(){
+   return studA[Math.floor(Math.random()*studA.length)].StudentLastname;
+}
+
+function getRandomFromB(){
+   return studB[Math.floor(Math.random()*studB.length)].StudentLastname;
+}
+
 // Kicthen
 function checkKitch(Lastname){
-    if (Kitch.some(e => e.LastName === `${Lastname}`)) {
-        return true;
-    }else{
-       return false;
-    }
+   if (Kitch.some(e => e.LastName === `${Lastname}`)) {
+       return true;
+   }else{
+      return false;
+   }
 }
 // Back Garden
 function checkBG(Lastname){
-    if (BG.some(e => e.LastName === `${Lastname}`)) {
-        return true;
-    }else{
-       return false;
-    }
+   if (BG.some(e => e.LastName === `${Lastname}`)) {
+       return true;
+   }else{
+      return false;
+   }
 }
 // Comlab
 function checkComp(Lastname){
-    if (Comp.some(e => e.LastName === `${Lastname}`)) {
-        return true;
-    }else{
-       return false;
-    }
+   if (Comp.some(e => e.LastName === `${Lastname}`)) {
+       return true;
+   }else{
+      return false;
+   }
 }
 // Courtyard
 function checkCY(Lastname){
-    if (CY.some(e => e.LastName === `${Lastname}`)) {
-        return true;
-    }else{
-       return false;
-    }
+   if (CY.some(e => e.LastName === `${Lastname}`)) {
+       return true;
+   }else{
+      return false;
+   }
 }
 // Dining
 function checkDin(Lastname){
-    if (Din.some(e => e.LastName === `${Lastname}`)) {
-        return true;
-    }else{
-       return false;
-    }
+   if (Din.some(e => e.LastName === `${Lastname}`)) {
+       return true;
+   }else{
+      return false;
+   }
 }
 // Dishwasher
 function checkDish(Lastname){
-    if (Dish.some(e => e.LastName === `${Lastname}`)) {
-        return true;
-    }else{
-       return false;
-    }
+   if (Dish.some(e => e.LastName === `${Lastname}`)) {
+       return true;
+   }else{
+      return false;
+   }
 }
 // Front Garden
 function checkFG(Lastname){
-    if (FG.some(e => e.LastName === `${Lastname}`)) {
-        return true;
-    }else{
-       return false;
-    }
+   if (FG.some(e => e.LastName === `${Lastname}`)) {
+       return true;
+   }else{
+      return false;
+   }
 }
 // Hallway
 function checkHall(Lastname){
-    if (Hall.some(e => e.LastName === `${Lastname}`)) {
-        return true;
-    }else{
-       return false;
-    }
+   if (Hall.some(e => e.LastName === `${Lastname}`)) {
+       return true;
+   }else{
+      return false;
+   }
 }
 // Laundry
 function checkLaun(Lastname){
-    if (Laun.some(e => e.LastName === `${Lastname}`)) {
-        return true;
-    }else{
-       return false;
-    }
+   if (Laun.some(e => e.LastName === `${Lastname}`)) {
+       return true;
+   }else{
+      return false;
+   }
 }
-// Office 
+// Office
 function checkOff(Lastname){
-    if (off.some(e => e.LastName === `${Lastname}`)) {
-        return true;
-    }else{
-       return false;
-    }
+   if (off.some(e => e.LastName === `${Lastname}`)) {
+       return true;
+   }else{
+      return false;
+   }
 }
 // Window 1
 function checkWin1(Lastname){
-    if (win1.some(e => e.LastName === `${Lastname}`)) {
-        return true;
-    }else{
-       return false;
-    }
+   if (win1.some(e => e.LastName === `${Lastname}`)) {
+       return true;
+   }else{
+      return false;
+   }
 }
 // Window 2
 function checkWin2(Lastname){
-    if (win2.some(e => e.LastName === `${Lastname}`)) {
-        return true;
-    }else{
-       return false;
-    }
+   if (win2.some(e => e.LastName === `${Lastname}`)) {
+       return true;
+   }else{
+      return false;
+   }
 }
+
+
