62a9cd9968630952f2089da39bb537707f352513
ALINGASA <alingasada_sd203a1@CEACCS.NET>
2019-05-16 16:51:02 +0800

    files modified

--- public/controller/shuffleStudent.js
+++ public/controller/shuffleStudent.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
           success:function(dat){
              console.log(dat);
              top.location.href = "/shuffledTask";
-          }
+           }
          })
       }
     });
